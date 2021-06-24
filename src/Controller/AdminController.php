5d83ade0c3e5154921bf6a481282753765e8caad
Madické THIANE <madicke.thiane@gmail.com>
2021-06-24 17:29:04 +0200

    Ajout du bouton supprimer

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,7 +26,32 @@ class AdminController extends AbstractController
         ]);
     }
 
-    public function deleteProduct()
+    /**
+     * @Route("/delete/{produitId}",name="delete_product")
+     */
+    public function deleteProduct(Request $request, $produitId)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $produitRepository = $entityManager->getRepository(Produit::class);
+        $produit = $produitRepository->find($produitId);
+        if (!$produit) {
+            return $this->redirect($this->generateUrl('admin_dashboard'));
+        }
+        $entityManager->remove($produit);
+        $entityManager->flush();
+        return $this->redirect($this->generateUrl('admin_dashboard'));
     }
+
+
+    // Formulaire : 
+
+    // /**
+    //  * @Route("/create",name="create_product")
+    //  */
+    // createProduct(Request $request,){}
+
+    // /**
+    //  * @Route("",name="edit_product")
+    //  */
+    // editProduct(Request $request,){}
 }
