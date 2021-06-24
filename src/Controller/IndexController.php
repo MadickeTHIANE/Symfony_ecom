4c174c04d488669adcd1855bfc61caad69bb64f3
Madické THIANE <madicke.thiane@gmail.com>
2021-06-24 15:28:04 +0200

    Correction

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -28,18 +28,32 @@ class IndexController extends AbstractController
     }
 
     /**
-     * @Route("/category/{categoryName}", name="index_category")
+     * @Route("/category/display/{categoryName}", name="index_category")
      */
-    public function indexCategory(Request $request, $categoryName): Response
+    public function indexCategory(Request $request, $categoryName = false): Response
     {
         //Cette fonction a pour but d'afficher la page d'accueil mais uniquement avec les éléments qui correspondent à la catégorie indiquée
         //Elle doit être disponible via les différentes options du menu déroulant Categories
+        //Nous récupérons le Repository de l'Entity dont nous avions besoin, laquelle est Category
         $entityManager = $this->getDoctrine()->getManager();
         $categoryRepository = $entityManager->getRepository(Category::class);
-        $category = $categoryRepository->findOneByName($categoryName);
-        $produits = $category->getProduits();
+        //Nous récupérons la liste des catégories via le Repository afin d'afficher la liste dans la navbar
+        $categories = $categoryRepository->findAll();
+        //Nous récupérons la Category dont le nom correspond à la valeur de la variable $categoryName
+        //La fonction findOneByName nous permet de récupérer UNE entrée dont le nom correspond à la valeur de l'attribut désigné: ici "name"
+        $selectedCategory = $categoryRepository->findOneByName($categoryName);
+        //Si la catégori recherchée n'existe pas, nous revenons vers l'index
+        if (!$selectedCategory) {
+            return $this->redirect($this->generateUrl('index'));
+        }
+        //Nous récupérons la liste des produits liés à notre Category grâce à getProduit()
+        //Etant donné que nos deux Entity sont liées, nous récupérons la liste entière des produits liés à Category via son tableau $produits. Il suffit alors de récupérer et de transmettre ce tableau à Twig
+
+        $produits = $selectedCategory->getProduits();
+        //Nous transmettons nos variables à Twig
         return $this->render('index/index.html.twig', [
-            "produits" => $produits
+            "produits" => $produits,
+            'categories' => $categories
         ]);
     }
 }
