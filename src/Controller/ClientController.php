622aaf337cb7c1c59f7bc3c159c885e627486af2
Madické THIANE <madicke.thiane@gmail.com>
2021-06-28 20:33:29 +0200

    Configuration bouton Supprimer

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Entity\Commande;
 use App\Entity\Reservation;
 use Symfony\Component\HttpFoundation\Request;
@@ -48,6 +49,47 @@ class ClientController extends AbstractController
         return $this->render('client/client-dashboard.html.twig', [
             'commandes' => $commandes,
             'reservations' => $reservations,
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{commandeId}/{reservationId}",name="delete_reservation")
+     */
+    public function deleteReservation(Request $request, $commandeId, $reservationId)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $reservationRepository = $entityManager->getRepository(Reservation::class);
+        $produitRepository = $entityManager->getRepository(Produit::class);
+        $commandeRepository = $entityManager->getRepository(Commande::class);
+
+        $produits = $produitRepository->findAll();
+        $reservations = $reservationRepository->findAll();
+        $commande = $commandeRepository->find($commandeId);
+        $selectedReservation = $reservationRepository->find($reservationId);
+
+        //La Reservation ne peut être supprimé si le produit n'existe pas ou si la commande est validée
+        //! erreur
+        // $produit = $selectedReservation->getProduit();
+        // $statut = $produit->getStatut();
+
+        if (!$selectedReservation /*|| $statut == "Validée"*/) {
+            return $this->redirect($this->generateUrl('client_dashboard'));
+        }
+
+
+        $entityManager->remove($selectedReservation);
+        $entityManager->flush();
+
+        //La commande est supprimée si la dernière réservation a été supprimée, soit si la table Reservation est nulle
+        if (count($reservations) == 1) { //? pourquoi je dois le mettre à 1 ? pas actualisé ?
+            $entityManager->remove($commande);
+            $entityManager->flush();
+            //Effacer le bouton Valider
+        }
+
+        return $this->render('index/index.html.twig', [
+            "produits" => $produits,
         ]);
     }
 }
