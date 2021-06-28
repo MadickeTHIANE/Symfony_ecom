8f01650dd6650fc13ef0f35bcfb4fc155044f1c2
Madické THIANE <madicke.thiane@gmail.com>
2021-06-28 17:04:50 +0200

    Validation

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Commande;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,5 +24,21 @@ class ClientController extends AbstractController
         return $this->render('client/client-dashboard.html.twig', [
             'commandes' => $commandes,
         ]);
+    }
+
+    /**
+     * @Route("/valid/{commandId}",name="confirm_command")
+     */
+    public function confirmCommand(Request $request, $commandId)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $commandeRepository = $entityManager->getRepository(Commande::class);
+        $commandes = $commandeRepository->findAll();
+        $selectedCommande = $commandeRepository->find($commandId);
+        if (!$selectedCommande->getStatut() == "Panier") {
+            return $this->redirect($this->generateUrl('client_dashboard'));
+        }
+        $selectedCommande->setStatut("Validée");
+        return $this->render('client/client-dashboard.html.twig', ['commandes' => $commandes,]);
     }
 }
