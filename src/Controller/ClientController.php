f9d3d979fea05f2278f7ae94055e20bac0a831b7
Madické THIANE <madicke.thiane@gmail.com>
2021-06-28 16:50:07 +0200

    clientDashboard

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commande;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/client")
@@ -16,8 +17,11 @@ class ClientController extends AbstractController
      */
     public function clientDashboard(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $commandeRepository = $entityManager->getRepository(Commande::class);
+        $commandes = $commandeRepository->findAll();
         return $this->render('client/client-dashboard.html.twig', [
-            'controller_name' => 'ClientController',
+            'commandes' => $commandes,
         ]);
     }
 }
